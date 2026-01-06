abeaa3809357e2cc1ab5d662691b90f696a65712
Niel <nico_elmer@hotmail.com>
2026-01-06 09:25:40 +0100

    fix: only load plugin once by manual adding plugin to model field

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { connect, type RenderFieldExtensionCtx } from "datocms-plugin-sdk";
 import AssetLinkAddon from "./entrypoints/AssetLinkAddon";
 
-console.log("PLUGIN LOADED: index.tsx");
-
 function render(component: React.ReactNode) {
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>{component}</React.StrictMode>
-  );
+  ReactDOM.createRoot(document.getElementById("root")!).render(component);
 }
 
 connect({
-  overrideFieldExtensions(field: any) {
-    const ft = field?.attributes?.field_type;
-    if (ft === "file" || ft === "gallery") {
-      return { addons: [{ id: "assetLinkAddon" }] };
-    }
-    return undefined;
-  },
-
   manualFieldExtensions() {
     return [
       { id: "assetLinkAddon", name: "Asset link", type: "addon", fieldTypes: ["gallery", "file"] },
